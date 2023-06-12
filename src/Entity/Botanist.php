a74c2ddcf85c8a478e5e7db2a069c6786a1b6cd1
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2023-06-12 11:07:13 +0200

    Création des entités

---
+++ src/Entity/Botanist.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BotanistRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BotanistRepository::class)]
+class Botanist
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?bool $isVerified = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function isIsVerified(): ?bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
+
+        return $this;
+    }
+}
