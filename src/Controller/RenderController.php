78d6b9a8439f4355e3e52e8fc65402039688f130
Theo77-art <77727097+Theo77-art@users.noreply.github.com>
2023-06-14 17:04:58 +0200

    First page modif

--- src/Controller/RenderController.php
+++ src/Controller/RenderController.php
@@ -11,6 +11,12 @@ class RenderController extends AbstractController
     #[Route('/', name: 'landing_page')]
     public function landingRender(): Response
     {
-        return $this->render('landingPage.html.twig');
+        return $this->render('guest_template/landingPage.html.twig');
+    }
+
+    #[Route('/connection', name: 'connection_page')]
+    public function connectionRender(): Response
+    {
+        return $this->render('guest_template/connectionPage.html.twig');
     }
 }
