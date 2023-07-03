451159b747a0924c2b6aabef4549741cd4067964
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-07-03 11:12:44 +0200

    add new fixture & refactor fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -16,6 +16,7 @@ use App\Entity\Status;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory as Faker;
+use Faker\Generator;
 
 class AppFixtures extends Fixture
 {
@@ -24,6 +25,58 @@ class AppFixtures extends Fixture
         $faker = Faker::create('en_US');
 
         // Create types of Status
+        $status = $this->createStatuses($manager);
+
+        // Create 10 botanists
+        $botanists = $this->createBotanists($faker, $manager);
+
+        // Create 20 particular
+        $particulars = $this->createParticulars($faker, $manager);
+
+        // Create 50 certificates for botanists
+        $this->createCertificates($faker, $manager);
+
+        // Create 10 appointments
+        $appointments = $this->createAppts($faker, $manager, $status, $botanists, $particulars);
+
+        // Create 10 advices
+        $advices = $this->createAdvices($faker, $manager, $status, $botanists, $particulars);
+
+        // Create 20 plants
+        $plants = $this->createPlants($faker, $manager, $status, $appointments, $advices);
+
+        // Create 20 comments
+        $comments = $this->createComments($faker, $manager);
+
+        foreach ($plants as $plant) {
+            $photo = new Photo();
+            $photo
+                ->setThumbnail('https://picsum.photos/200/300')
+                ->setSmallThumbnail('https://picsum.photos/100/150')
+                ->setCreatedAt(new \DateTimeImmutable('now'))
+                ->setPlant($plant);
+
+            $manager->persist($photo);
+        }
+
+        foreach ($particulars as $particular) {
+            $address = new Address();
+            $address
+                ->setStreet($faker->streetAddress)
+                ->setZipCode($faker->randomNumber(5))
+                ->setCity($faker->city)
+                ->setCreatedAt(new \DateTimeImmutable())
+                ->setUpdatedAt(new \DateTimeImmutable())
+                ->setParticular($particular);
+
+            $manager->persist($address);
+        }
+
+        $manager->flush();
+    }
+
+    private function createStatuses(ObjectManager $manager): array
+    {
         $status = [];
         foreach (Status::STATUS as $value) {
             $oneStatus = new Status();
@@ -32,7 +85,11 @@ class AppFixtures extends Fixture
             $status[] = $oneStatus;
         }
 
-        // Create 10 botanists
+        return $status;
+    }
+
+    private function createBotanists(Generator $faker, ObjectManager $manager): array
+    {
         $botanists = [];
         for ($i = 0; $i < 10; ++$i) {
             $botanist = new Botanist();
@@ -43,14 +100,17 @@ class AppFixtures extends Fixture
                 ->setLastName($faker->lastName)
                 ->setRoles(['ROLE_USER'])
                 ->setCellphone($faker->phoneNumber);
-            // ->setIsVerified($faker->boolean);
 
             $manager->persist($botanist);
             $botanists[] = $botanist;
             $this->addReference(Botanist::class.'_'.$i, $botanist);
         }
 
-        // Create 20 particular
+        return $botanists;
+    }
+
+    private function createParticulars(Generator $faker, ObjectManager $manager): array
+    {
         $particulars = [];
         for ($i = 0; $i < 20; ++$i) {
             $particular = new Particular();
@@ -62,12 +122,18 @@ class AppFixtures extends Fixture
                 ->setRoles(['ROLE_USER'])
                 ->setCellphone($faker->phoneNumber);
 
+            $this->createAddress($faker, $manager, $particular);
+
             $manager->persist($particular);
             $particulars[] = $particular;
             $this->addReference(Particular::class.'_'.$i, $particular);
         }
 
-        // Create 50 certificates for botanists
+        return $particulars;
+    }
+
+    private function createCertificates(Generator $faker, ObjectManager $manager): void
+    {
         for ($i = 1; $i <= 50; ++$i) {
             $certificate = new Certificate();
             $certificate->setTitle($faker->sentence(4))
@@ -82,8 +148,10 @@ class AppFixtures extends Fixture
 
             $manager->persist($certificate);
         }
+    }
 
-        // Create 10 appointments
+    private function createAppts(Generator $faker, ObjectManager $manager, array $status, array $botanists, array $particulars): array
+    {
         $appointments = [];
         for ($i = 0; $i < 10; ++$i) {
             // Appointment
@@ -112,7 +180,11 @@ class AppFixtures extends Fixture
             $this->addReference(Appointment::class.'_'.$i, $appointment);
         }
 
-        // Create 10 advices
+        return $appointments;
+    }
+
+    private function createAdvices(Generator $faker, ObjectManager $manager, array $status, array $botanists, array $particulars): array
+    {
         $advices = [];
         for ($i = 0; $i < 10; ++$i) {
             // Advice
@@ -138,7 +210,11 @@ class AppFixtures extends Fixture
             $this->addReference(Advice::class.'_'.$i, $advice);
         }
 
-        // Create 20 plants
+        return $advices;
+    }
+
+    private function createPlants(Generator $faker, ObjectManager $manager, array $status, array $appointments, array $advices): array
+    {
         $plants = [];
 
         for ($i = 0; $i < 20; ++$i) {
@@ -166,12 +242,18 @@ class AppFixtures extends Fixture
                 ->addRequest($appointments[$i % 10])
                 ->addRequest($advices[$i % 10]);
 
+            $this->createPhoto($plant, $manager);
+
             $manager->persist($plant);
             $plants[] = $plant;
             $this->addReference(Plant::class.'_'.$i, $plant);
         }
 
-        // Create 20 comments
+        return $plants;
+    }
+
+    private function createComments(Generator $faker, ObjectManager $manager): array
+    {
         $comments = [];
         for ($i = 0; $i < 20; ++$i) {
             $comment = new Comment();
@@ -194,30 +276,32 @@ class AppFixtures extends Fixture
             $manager->persist($comment);
         }
 
-        foreach ($plants as $plant) {
-            $photo = new Photo();
-            $photo
-                ->setThumbnail('https://picsum.photos/200/300')
-                ->setSmallThumbnail('https://picsum.photos/100/150')
-                ->setCreatedAt(new \DateTimeImmutable('now'))
-                ->setPlant($plant);
+        return $comments;
+    }
 
-            $manager->persist($photo);
-        }
+    private function createPhoto(Plant $plant, ObjectManager $manager): void
+    {
+        $photo = new Photo();
+        $photo
+            ->setThumbnail('https://picsum.photos/200/300')
+            ->setSmallThumbnail('https://picsum.photos/100/150')
+            ->setCreatedAt(new \DateTimeImmutable('now'))
+            ->setPlant($plant);
 
-        foreach ($particulars as $particular) {
-            $address = new Address();
-            $address
-                ->setStreet($faker->streetAddress)
-                ->setZipCode($faker->randomNumber(5))
-                ->setCity($faker->city)
-                ->setCreatedAt(new \DateTimeImmutable())
-                ->setUpdatedAt(new \DateTimeImmutable())
-                ->setParticular($particular);
+        $manager->persist($photo);
+    }
 
-            $manager->persist($address);
-        }
+    private function createAddress(Generator $faker, ObjectManager $manager, Particular $particular): void
+    {
+        $address = new Address();
+        $address
+            ->setStreet($faker->streetAddress)
+            ->setZipCode($faker->randomNumber(5))
+            ->setCity($faker->city)
+            ->setCreatedAt(new \DateTimeImmutable())
+            ->setUpdatedAt(new \DateTimeImmutable())
+            ->setParticular($particular);
 
-        $manager->flush();
+        $manager->persist($address);
     }
 }
