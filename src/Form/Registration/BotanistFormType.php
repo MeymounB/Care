5e1666d671533823c4761edf1f156a713ebaac24
Jukiyoomi <ncjmw2506@gmail.com>
2023-07-10 16:36:17 +0200

    new form field

--- src/Form/Registration/BotanistFormType.php
+++ src/Form/Registration/BotanistFormType.php
@@ -3,10 +3,29 @@
 namespace App\Form\Registration;
 
 use App\Entity\Botanist;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
 
 class BotanistFormType extends UserFormType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        parent::buildForm($builder, $options);
+        $builder
+            ->add('certif', FileType::class, [
+                'mapped' => false,
+                'label' => 'Document de certification',
+                'required' => true,
+                'constraints' => [
+                    new File([
+                        'mimeTypes' => ['application/pdf'],
+                        'maxSize' => 1024 * 512 // max file size is
+                    ])
+                ]
+            ]);
+    }
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
