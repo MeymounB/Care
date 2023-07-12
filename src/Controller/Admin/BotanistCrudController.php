b9c1a8e07d1ae64d39464dc777011b5b5ce48058
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2023-07-12 12:06:26 +0200

    Add switch to is_verified

--- src/Controller/Admin/BotanistCrudController.php
+++ src/Controller/Admin/BotanistCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Botanist;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -20,6 +21,7 @@ class BotanistCrudController extends AbstractCrudController
             IdField::new('id')
                 ->hideWhenCreating()
                 ->hideWhenUpdating(),
+            BooleanField::new('is_verified'),
             TextField::new('FullName')->setLabel('Full Name'),
             TextField::new('email'),
             // TextField::new('request', 'Have taken request'),
