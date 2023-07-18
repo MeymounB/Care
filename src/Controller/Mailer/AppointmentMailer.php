d7eb8c153c55e3d931df365b791477ef65f14140
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2023-07-18 12:04:54 +0200

    Update AppointmentMailer.php

--- src/Controller/Mailer/AppointmentMailer.php
+++ src/Controller/Mailer/AppointmentMailer.php
@@ -1,30 +1,30 @@
 <?php
-
-namespace App\Mailer;
-
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
-use Symfony\Component\Mailer\MailerInterface;
-
-class AppointmentMailer
-{
-    // private $mailer;
-
-    // public function __construct(MailerInterface $mailer)
-    // {
-    //     $this->mailer = $mailer;
-    // }
-
-    // public function sendAppointmentConfirmation($appointment)
-    // {
-    //     $email = (new TemplatedEmail())
-    //         ->from('hello@example.com')
-    //         ->to($appointment->getUser()->getEmail())
-    //         ->subject('Confirmation de votre demande')
-    //         ->htmlTemplate('emails/appt_confirmation.html.twig')
-    //         ->context([
-    //             'Appointment' => $appointment,
-    //         ]);
-
-    //     $this->mailer->send($email);
-    // }
-}
+//
+//namespace App\Mailer;
+//
+//use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+//use Symfony\Component\Mailer\MailerInterface;
+//
+//class AppointmentMailer
+//{
+//     private $mailer;
+//
+//     public function __construct(MailerInterface $mailer)
+//     {
+//         $this->mailer = $mailer;
+//     }
+//
+//     public function sendAppointmentConfirmation($appointment)
+//     {
+//         $email = (new TemplatedEmail())
+//             ->from('hello@example.com')
+//             ->to($appointment->getUser()->getEmail())
+//             ->subject('Confirmation de votre demande')
+//             ->htmlTemplate('emails/appt_confirmation.html.twig')
+//             ->context([
+//                 'Appointment' => $appointment,
+//             ]);
+//
+//         $this->mailer->send($email);
+//     }
+//}
