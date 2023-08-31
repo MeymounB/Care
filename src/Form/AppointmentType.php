fcbdc8971d69d2d3f05f30575555a6d32d6ac86e
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-08-31 10:25:05 +0200

    update appointment form (add mor config & remove fields)

--- src/Form/AppointmentType.php
+++ src/Form/AppointmentType.php
@@ -6,26 +6,45 @@ use App\Entity\Appointment;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Range;
 
 class AppointmentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class)
-            ->add('description', null, [
+            ->add('title', TextType::class, [
+                "label" => "Titre",
+                'error_bubbling' => true,
+                'required' => true
+            ])
+            ->add('description', TextareaType::class, [
+                "label" => "Description",
+                'required' => true,
                 'attr' => [
                     'rows' => 10,
                 ],
+                'error_bubbling' => true,
+
             ])
 
             ->add('plannedAt', DateTimeType::class, [
+                'label' => "Date souhaitée",
                 'widget' => 'single_text',
                 'html5' => false,
+                'required' => true,
                 'format' => 'yyyy-MM-dd HH:mm',
+                'error_bubbling' => true,
+                'constraints' => [
+                    new Range([
+                        'min' => 'now',
+                        'minMessage' => 'La date doit être supérieure à la date actuelle',
+                    ])
+                ]
             ])
 
             ->add('isPresential', ChoiceType::class, [
@@ -33,16 +52,17 @@ class AppointmentType extends AbstractType
                     'Présentiel' => true,
                     'Distanciel' => false,
                 ],
-                'expanded' => true,
+                // 'expanded' => true,
                 'label' => 'Vous préférez un rendez-vous :',
+                'error_bubbling' => true,
+                'required' => true
             ])
 
-            ->add('address', TextType::class, [
-                'required' => false,
-            ])
-            ->add('link', TextType::class, [
-                'required' => false,
-            ]);
+            // ->add('address', TextType::class, [
+            //     'required' => false,
+            //     "label" => "Adresse"
+            // ])
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
