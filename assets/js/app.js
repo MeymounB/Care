f8c0841463a0b870f67521ed7f4cd94f5e73ccaf
Noodles-boop <72260596+DevDeNiro@users.noreply.github.com>
2023-09-03 13:16:30 +0200

    rename file js

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@ import {
 } from "./components/appointmentForm";
 
 import {initShowModal, initDeleteForm} from "./components/adviceForm";
-import {initDeleteAccountPopup} from "./components/delete_account_popup";
+import {initDeleteAccountPopup} from "./components/deleteAccountPopup";
 
 import {initImageSelection, deleteImage} from "./modules/imageSelection";
 import {initProfileSettingsButton} from "./modules/profileSettings";
@@ -13,14 +13,14 @@ import {initHorizontalScroll} from "./modules/horizontalScroll";
 import {initDropdown} from "./modules/dropdown";
 import {initSearch} from "./modules/search";
 
-// components
+/* components */
 initShowModal();
 initDeleteForm();
 initIsPresential();
 initDatetimeValidation();
 initDeleteAccountPopup();
 
-// modules
+/* modules */
 // initImageSelection();
 // initProfileSettingsButton();
 // initAppointmentRequest();
