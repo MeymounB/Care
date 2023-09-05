c69d8c2a111e27d628f9d0196faa901ccba5c219
Noodles-boops <Oldoni.jeremy@outlook.fr>
2023-09-05 09:49:59 +0200

    fix loop img

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,21 +6,23 @@ import {
 import {initShowModal, initDeleteForm} from "./components/adviceForm";
 import {initDeleteAccountPopup} from "./components/deleteAccountPopup";
 
+/*
 import {initImageSelection, deleteImage} from "./modules/imageSelection";
 import {initProfileSettingsButton} from "./modules/profileSettings";
 import {initAppointmentRequest} from "./modules/appointmentRequest";
 import {initHorizontalScroll} from "./modules/horizontalScroll";
 import {initDropdown} from "./modules/dropdown";
 import {initSearch} from "./modules/search";
+*/
 
-/* components */
+/* COMPONENTS */
 initShowModal();
 initDeleteForm();
 initIsPresential();
 initDatetimeValidation();
 initDeleteAccountPopup();
 
-/* modules */
+/* MODULES */
 // initImageSelection();
 // initProfileSettingsButton();
 // initAppointmentRequest();
