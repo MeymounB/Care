7d67c0398eea31f46c71eee50b2b32c2e2a1cb7b
Noodles-boop <72260596+DevDeNiro@users.noreply.github.com>
2023-09-09 13:58:40 +0200

    👌 IMPROVE: change structure of Js files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,5 @@
-import {
-  initIsPresential,
-  initDatetimeValidation,
-} from "./components/appointmentForm";
+import * as Components from "./components";
+import * as Modules from "./modules";
 
-import {initShowModal, initDeleteForm} from "./components/adviceForm";
-import {initDeleteAccountPopup} from "./components/deleteAccountPopup";
-import {
-  initPlantEditFormPopup,
-  initPlantAddFormPopup,
-  handleFileSelect,
-} from "./components/plantFormPopup";
-
-/*
-import {initImageSelection, deleteImage} from "./modules/imageSelection";
-import {initProfileSettingsButton} from "./modules/profileSettings";
-import {initAppointmentRequest} from "./modules/appointmentRequest";
-import {initHorizontalScroll} from "./modules/horizontalScroll";
-import {initDropdown} from "./modules/dropdown";
-import {initSearch} from "./modules/search";
-*/
-
-/* COMPONENTS */
-initPlantEditFormPopup();
-initPlantAddFormPopup();
-handleFileSelect();
-initShowModal();
-initDeleteForm();
-initIsPresential();
-initDatetimeValidation();
-initDeleteAccountPopup();
-
-/* MODULES */
-// initImageSelection();
-// initProfileSettingsButton();
-// initAppointmentRequest();
-// initHorizontalScroll();
-// initDropdown();
-// initSearch();
+Components.init();
+// Modules.init();
