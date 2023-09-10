17ea0bf97f5b0a2195493efa95dfeb7286c1b5c7
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-09-10 11:16:50 +0200

    update route

--- src/Controller/ImageDisplayController.php
+++ src/Controller/ImageDisplayController.php
@@ -2,24 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
-use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class ImageDisplayController extends AbstractController
 {
 	#[Route('/image/{imageName}', name: 'app_image_display', methods: ['GET'])]
-	public function showImageAction(string $imageName): BinaryFileResponse
+	public function showImageAction(#[CurrentUser] ?User $user, string $imageName): BinaryFileResponse
 	{
-		$response = new BinaryFileResponse(
+		if (!$user instanceof User) {
+			throw $this->createAccessDeniedException('Access denied');
+		}
+
+		return new BinaryFileResponse(
 			$this->getParameter('photos_dir')
 			. '/'
 			. $imageName
 		);
-		$response->setContentDisposition(
-			ResponseHeaderBag::DISPOSITION_ATTACHMENT
-		);
-		return $response;
 	}
 }
